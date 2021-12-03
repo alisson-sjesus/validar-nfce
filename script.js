32e04ad71ae7c0c423d59b17357a99ff872c41b5
Alisson Santos <alisson.sjesus@outlook.com>
2021-12-02 22:56:53 -0300

    adicionado aviso de copy e animacao

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ const nfce = document.querySelector('.nfce');
 const output = document.querySelector('.output');
 const btnCopiar = document.querySelector('.btn-copiar');
 const btn = document.querySelector('.btn');
+const aviso = document.querySelector('.aviso');
 const input = nfce.value;
+
+function handleClickBtn() {}
+
+btn.addEventListener('click', handleClickBtn);
 
 btnCopiar.addEventListener('click', () => {
   window.navigator.clipboard.writeText(output.innerText);
+  aviso.style.display = 'block';
+
+  setTimeout(() => {
+    aviso.style.display = 'none';
+  }, 2500);
 });
